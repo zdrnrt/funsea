eb7ab8a5eeb471f6d907c3032ef869c9ce0140e1
Задорин Артем <artfan95@gmail.com>
2026-01-12 23:15:36 +0700

    init: Начало работы, разметка блоков header и footer

---
+++ vite.config.js
@@ -0,0 +1,55 @@
+import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { resolve } from 'node:path'
+import includeHtml from 'vite-plugin-include-html'
+
+const preparedStaticHtml = () => {
+  return {
+    name: 'prepare-static-html',
+    apply: 'build',
+    transformIndexHtml(html) {
+      return html.replace(/\s*crossorigin(\s*=\s*("|')[^"']*\2)?/g, '').replaceAll('type="module"', 'defer');
+    },
+  };
+};
+
+export default defineConfig({
+  base: '',
+  plugins: [
+    includeHtml(),
+    preparedStaticHtml(),
+    ViteImageOptimizer({
+      cache: true,
+      includePublic: true,
+      svg: {
+        plugins: [
+          'preset-default',
+          'sortAttrs',
+        ],
+      },
+    }),
+  ],
+  build: {
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|webp|avif/i.test(extType)) {
+            return `assets/img/[name]-[hash][extname]`;
+          }
+          if (/woff2?|ttf|otf|eot/i.test(extType)) {
+            return `assets/fonts/[name]-[hash][extname]`;
+          }
+          if (/css/i.test(extType)) {
+            return `assets/css/[name]-[hash][extname]`;
+          }
+          return `assets/[ext]/[name]-[hash][extname]`;
+        },
+      }
+    },
+  }
+});
