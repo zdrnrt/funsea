08e1614f3d57e69655e1d85035e3caa523889f3f
Задорин Артем <artfan95@gmail.com>
2026-01-28 22:44:35 +0700

    feat: Добавлен функционал табов

---
+++ src/js/components/tabs.js
@@ -0,0 +1,24 @@
+if (document.querySelector('.tabs')) {
+	function tabToggle(e) {
+		const target = e.currentTarget.dataset.targetTab;
+		const tabTarget = document.querySelector(`[data-tab="${target}"]`);
+		if (tabTarget.classList.contains('tabs__item--open')) {
+			return;
+		}
+		const tabContainer = e.currentTarget.closest('.js-tabs');
+		console.log(tabContainer);
+		tabContainer
+			.querySelector('.tabs__btn--active')
+			.classList.remove('tabs__btn--active');
+		e.currentTarget.classList.add('tabs__btn--active');
+
+		tabContainer
+			.querySelector('.tabs__item--open')
+			.classList.remove('tabs__item--open');
+		tabTarget.classList.add('tabs__item--open');
+	}
+
+	for (const tabTrigger of document.querySelectorAll('.js-tab-trigger')) {
+		tabTrigger.addEventListener('click', tabToggle);
+	}
+}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import 'normalize.css'
 import './styles/main.scss'
 import './js/components/modals'
+import './js/components/tabs'
