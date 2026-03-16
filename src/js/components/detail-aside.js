3b0ee93e4647b84d897ab5fb195460fe4f959659
Задорин Артем <artfan95@gmail.com>
2026-03-17 00:23:42 +0700

    feat: Доработан виджет бронирования

---
+++ src/js/components/detail-aside.js
@@ -0,0 +1,18 @@
+if (document.querySelector('.js-detail-aside')){
+  const aside = document.querySelector('.js-detail-aside');
+  function clickOutsideAside(e){
+    if (!e.target.closest('.detail-aside__container')){
+      toggleExpandDetailAside()
+    }
+  }
+  function toggleExpandDetailAside(){
+    aside.classList.toggle('detail-aside--expand');
+    document.body.classList.toggle('detail-aside--body-expand');
+    if (aside.classList.contains('detail-aside--expand')){
+      setTimeout(() => {document.body.addEventListener('click', clickOutsideAside)}, 0)
+    } else {
+      document.body.removeEventListener('click', clickOutsideAside)
+    }
+  }
+  document.querySelector('.js-detail-aside-trigger').addEventListener('click', toggleExpandDetailAside)
+}
