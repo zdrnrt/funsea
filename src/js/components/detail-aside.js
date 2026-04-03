e907c30b3dc89237bcc825dda543206673598cd3
Задорин Артем <artfan95@gmail.com>
2026-04-03 22:27:49 +0700

    fix: Мелкие правки верстки

--- src/js/components/detail-aside.js
+++ src/js/components/detail-aside.js
@@ -1,18 +1,18 @@
-if (document.querySelector('.js-detail-aside')){
+function clickOutsideAside(e){
+  if (!e.target.closest('.detail-aside__container')){
+    toggleExpandDetailAside()
+  }
+}
+function toggleExpandDetailAside(){
   const aside = document.querySelector('.js-detail-aside');
-  function clickOutsideAside(e){
-    if (!e.target.closest('.detail-aside__container')){
-      toggleExpandDetailAside()
-    }
+  aside.classList.toggle('detail-aside--expand');
+  document.body.classList.toggle('detail-aside--body-expand');
+  if (aside.classList.contains('detail-aside--expand')){
+    setTimeout(() => {document.body.addEventListener('click', clickOutsideAside)}, 0)
+  } else {
+    document.body.removeEventListener('click', clickOutsideAside)
   }
-  function toggleExpandDetailAside(){
-    aside.classList.toggle('detail-aside--expand');
-    document.body.classList.toggle('detail-aside--body-expand');
-    if (aside.classList.contains('detail-aside--expand')){
-      setTimeout(() => {document.body.addEventListener('click', clickOutsideAside)}, 0)
-    } else {
-      document.body.removeEventListener('click', clickOutsideAside)
-    }
-  }
+}
+if (document.querySelector('.js-detail-aside-trigger')){
   document.querySelector('.js-detail-aside-trigger').addEventListener('click', toggleExpandDetailAside)
 }
